ec20e16a53188b40fb26a43127f7a78719f2d94c
LucyC-1225 <lucyc1225xx@gmail.com>
2025-06-16 14:54:16 -0400

    cors enabling

--- app.py
+++ app.py
@@ -10,20 +10,8 @@ def hello():
     return jsonify(message="Hello from Flask!")
 
 # gets a single recipe from the mealdb API
-@app.route("/get-recipe", methods=["GET", "OPTIONS"])
+@app.route("/get-recipe", methods=["GET"])
 def get_recipe():
-    # CORS headers
-    headers = {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, POST",
-        "Access-Control-Allow-Headers": "*",
-        "Access-Control-Max-Age": "3600",
-    }
-
-    # Handle preflight
-    if request.method == "OPTIONS":
-        return ("", 204, headers)
-
     # Make API request to TheMealDB
     response = requests.get("https://www.themealdb.com/api/json/v1/1/random.php")
 
